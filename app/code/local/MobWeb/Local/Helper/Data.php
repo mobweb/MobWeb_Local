ba99d6bcbdd38cfac4983e8df3d6dec8abd1d569
Louis Bataillard <info@mobweb.ch>
2016-04-04 14:40:07 +0200

    initial commit

---
+++ app/code/local/MobWeb/Local/Helper/Data.php
@@ -0,0 +1,20 @@
+<?php
+
+class MobWeb_Local_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    public function getDevelopmentEnvironment() {
+
+        // Match the base URL against known strings indicating a certain development environment
+        $baseUrl = Mage::getBaseUrl();
+
+        if((strpos($baseUrl, '.local') !==  false) (strpos($baseUrl, '.dev') !==  false) || (strpos($baseUrl, 'development') !==  false)) {
+            return 'development';
+        }
+
+        if(strpos($baseUrl, 'staging') !==  false) {
+            return 'staging';
+        }
+
+        return 'production';
+    }
+}
